f39fffe47da1ef440de2aa2a0466f8a3ab82d95f
Filip Krawczyk <kotmawszy@gmail.com>
2024-08-16 10:23:14 +0200

    Update transaction sending script

--- cli/verify.sh
+++ cli/verify.sh
@@ -6,14 +6,36 @@ if [ $# -ne 1 ]; then
 fi
 
 send_transaction() {
-    sncast \
-        --wait \
-        invoke \
-        --contract-address "$(<calldata/contract_address)" \
-        --function "$1" \
-        --calldata "$3 $(<$2)"
+    local retries=5
+    local count=0
+    local status=0
+
+    while [[ $count -lt $retries ]]; do
+        sncast \
+            --wait \
+            invoke \
+            --contract-address "$(<calldata/contract_address)" \
+            --function "$1" \
+            --calldata "$3 $(<$2)"
+
+        sleep 5 # extra delay to make sure the transaction is registered
+
+        status=$?
+
+        if [[ $status -eq 0 ]]; then
+            return 0
+        else
+            echo "Transaction failed with status $status. Retrying... ($((count + 1))/$retries)"
+        fi
+
+        count=$((count + 1))
+    done
+
+    echo "Transaction failed after $retries attempts."
+    return $status
 }
 
+echo ""
 echo "Sending verify_proof_initial"
 send_transaction "verify_proof_initial" "calldata/initial" $1
 
@@ -23,6 +45,7 @@ while true; do
     filename="calldata/step${i}"
 
     if [[ -e "$filename" ]]; then
+        echo ""
         echo "Sending verify_proof_step (${i})"
         send_transaction "verify_proof_step" "$filename" "$job_id"
     else
@@ -32,5 +55,6 @@ while true; do
     ((i++))
 done
 
-echo "Sending verify_proof_final"
-send_transaction "verify_proof_final" "calldata/final" $1
+echo ""
+echo "Sending verify_proof_final_and_register_fact"
+send_transaction "verify_proof_final_and_register_fact" "calldata/final" $1
