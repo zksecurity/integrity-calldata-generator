fd7f1fd2d91d8618df3be8d236b0596cedd18c3e
jason <94618524+mellowcroc@users.noreply.github.com>
2024-09-24 21:32:02 -0500

    feat: add `parse_as_exprs` function

--- proof_parser/src/lib.rs
+++ proof_parser/src/lib.rs
@@ -1,4 +1,5 @@
 mod annotations;
+mod ast;
 mod builtins;
 mod conversion;
 mod json_parser;
@@ -6,13 +7,23 @@ mod layout;
 mod stark_proof;
 mod utils;
 
-use crate::{json_parser::ProofJSON, stark_proof::StarkProof};
+pub use crate::json_parser::ProofJSON;
+pub use crate::stark_proof::{
+    CairoPublicInput, StarkConfig, StarkProof, StarkUnsentCommitment, StarkWitness,
+};
 use std::convert::TryFrom;
 extern crate clap;
 extern crate num_bigint;
 extern crate regex;
 extern crate serde;
-use swiftness_stark::types::StarkProof as StarkProofFromVerifier;
+use crate::ast::Exprs;
+
+pub struct ParseStarkProof {
+    pub config: Exprs,
+    pub public_input: Exprs,
+    pub unsent_commitment: Exprs,
+    pub witness: Exprs,
+}
 
 pub fn parse(input: String) -> anyhow::Result<StarkProofFromVerifier> {
     let proof_json = serde_json::from_str::<ProofJSON>(&input)?;
@@ -21,6 +32,16 @@ pub fn parse(input: String) -> anyhow::Result<StarkProofFromVerifier> {
     Ok(stark_proof_verifier)
 }
 
+pub fn parse_as_exprs(input: String) -> anyhow::Result<ParseStarkProof> {
+    let proof_json = serde_json::from_str::<ProofJSON>(&input)?;
+    let stark_proof = StarkProof::try_from(proof_json)?;
+    Ok(ParseStarkProof {
+        config: Exprs::from(stark_proof.config),
+        public_input: Exprs::from(stark_proof.public_input),
+        unsent_commitment: Exprs::from(stark_proof.unsent_commitment),
+        witness: Exprs::from(stark_proof.witness),
+    })
+}
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -31,5 +52,11 @@ mod tests {
         let proof_json = serde_json::from_str::<ProofJSON>(input).unwrap();
         let stark_proof = StarkProof::try_from(proof_json).unwrap();
         let _: StarkProofFromVerifier = stark_proof.into();
+    }
+
+    #[test]
+    fn test_parse_as_exprs() {
+        let input = include_str!("../../examples/proofs/recursive/cairo0_example_proof.json");
+        let _: ParseStarkProof = parse_as_exprs(input).unwrap();
     }
 }
